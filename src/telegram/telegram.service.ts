9bc973bd3a8491ca4b9f6f4a5137dfda0b04e469
javdamonazzam <m131037@gmail.com>
2025-04-13 13:52:17 +0330

    a

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -6,7 +6,7 @@ import * as QRCode from 'qrcode';
 @Injectable()
 export class TelegramService implements OnModuleInit {
     private bot: Telegraf;
-
+    private userServiceType: Map<number, string> = new Map();
     constructor(
 
     ) {
@@ -14,16 +14,16 @@ export class TelegramService implements OnModuleInit {
     }
 
     async onModuleInit() {
-        this.bot.start(async (ctx) => {
+        this.bot.start(async (ctx) => { 
             const chatId = ctx.message.chat.id;
-            const res = await axios.post('http://localhost:8001/telegram/start', {
+            const res = await axios.post('http://84.200.154.221:8001/telegram/start', {
                 "chatId": chatId
             });
             await ctx.reply(`${chatId}`)
             await ctx.reply(
                 'یکی از گزینه‌های زیر را انتخاب کنید:',
                 Markup.keyboard([
-                    ['🔄 تمدید سرویس', '🛒 خرید سرویس'],
+                    ['🌐 WireGuard', '🔐 OpenVPN'],
 
                     ['تعویض سرویس']
                 ]).resize()
@@ -31,9 +31,11 @@ export class TelegramService implements OnModuleInit {
         });
 
         // ✅ پردازش خرید سرویس 
-        this.bot.hears('🛒 خرید سرویس', async (ctx) => {
+        this.bot.hears(  ['🌐 WireGuard', '🔐 OpenVPN'], async (ctx) => {
+            const selected = ctx.message.text.includes('WireGuard') ? 'wireguard' : 'openvpn';
             const chatId = ctx.message.chat.id;
-            const res = await axios.post('http://localhost:8001/telegram/balance', {
+            this.userServiceType.set(chatId, selected);
+            const res = await axios.post('http://84.200.154.221:8001/telegram/balance', {
                 "chatId": chatId
             });
             await ctx.reply(
@@ -49,12 +51,12 @@ export class TelegramService implements OnModuleInit {
         // ✅ پردازش مدت زمان اشتراک
         this.bot.hears(["1️⃣ یک‌ماهه", "2️⃣ دوماهه", "3️⃣ سه‌ماهه", "6️⃣ شش‌ماهه"], async (ctx) => {
             const chatId = ctx.message.chat.id;
+            const serviceType = this.userServiceType.get(chatId) || 'wireguard'; 
+            console.log(serviceType);
 
-            const res = await axios.post('http://localhost:8001/telegram/balance', {
+            const res = await axios.post('http://84.200.154.221:8001/telegram/balance', {
                 "chatId": chatId
             });
-
-
             let months = 1;
             switch (ctx.message.text) {
                 case "2️⃣ دوماهه": months = 2; break;
@@ -70,36 +72,54 @@ export class TelegramService implements OnModuleInit {
                 return;
             }
             await ctx.reply("⏳ لطفاً منتظر بمانید، در حال ساخت کانفیگ...");
-            //   const config = await axios.post('http://localhost:5000/tel/create',{ ip: '213.159.73.194', id: res.data.data.user_id, month: months, title: "b" });
-            console.log(res.data.data.user.id);
-            const res1 = await axios.post('http://localhost:8001/telegram/config', {
-                ip: '213.159.73.194',
-                id: res.data.data.user.id,
-                month: months,
-                title: "b"
-            });
-
-            const config = res1.data.data
-
-            const buffer = Buffer.from(config.server_info, 'utf-8');
-            await ctx.replyWithDocument({ source: buffer, filename: `${config.title}.conf` });
-
-            const qrBuffer = await QRCode.toBuffer(config.server_info);
-            await ctx.replyWithPhoto({ source: qrBuffer });
-
-            await ctx.reply("✅ کانفیگ شما ساخته شد! برای اطلاعات بیشتر به پشتیبانی پیام دهید.");
+            if (serviceType === 'openvpn') {
+                const res1 = await axios.post('http://84.200.154.221:8001/telegram/config', {
+                    ip: '192.142.18.225',
+                    id: res.data.data.user.id,
+                    month: months,
+                    title: "bop"
+                });
+                const config = res1.data.data
+    
+                const buffer = Buffer.from(config.server_info, 'utf-8');
+                await ctx.replyWithDocument({ source: buffer, filename: `${config.title}.ovpn` });
+                await ctx.reply("✅ کانفیگ شما ساخته شد! برای اطلاعات بیشتر به پشتیبانی پیام دهید.");
+                
+            } else {
+                console.log("start wiregurd");
+                const res1 = await axios.post('http://84.200.154.221:8001/telegram/config', {
+                    ip: '213.159.73.194',
+                    id: res.data.data.user.id,
+                    month: months,
+                    title: "b"
+                });
+                const config = res1.data.data
+    
+                const buffer = Buffer.from(config.server_info, 'utf-8');
+                await ctx.replyWithDocument({ source: buffer, filename: `${config.title}.conf` });
+    
+                const qrBuffer = await QRCode.toBuffer(config.server_info);
+                await ctx.replyWithPhoto({ source: qrBuffer });
+    
+                await ctx.reply("✅ کانفیگ شما ساخته شد! برای اطلاعات بیشتر به پشتیبانی پیام دهید.");
+                
+            }
         });
         this.bot.hears("❌ لغو", async (ctx) => {
-            const chatId = ctx.message.chat.id;
-            await ctx.reply(
-                'یکی از گزینه‌های زیر را انتخاب کنید:',
-                Markup.keyboard([
-                    //   [`💰 ${user_wallet.wallet_balance.toLocaleString('fa-IR')} هزارتومان`],
-                    ['🔄 تمدید سرویس', '🛒 خرید سرویس'],
-                    ['تعویض سرویس']
-                ]).resize()
-            );
-        });
+                const chatId = ctx.message.chat.id;
+                const res = await axios.post('http://84.200.154.221:8001/telegram/start', {
+                    "chatId": chatId
+                });
+                await ctx.reply(`${chatId}`)
+                await ctx.reply(
+                    'یکی از گزینه‌های زیر را انتخاب کنید:',
+                    Markup.keyboard([
+                        ['🌐 WireGuard', '🔐 OpenVPN'],
+    
+                        ['تعویض سرویس']
+                    ]).resize()
+                );
+            });
         // ✅ پردازش تمدید سرویس
         this.bot.hears('🔄 تمدید سرویس', async (ctx) => {
             await ctx.reply('🔄 لطفاً اطلاعات تمدید را وارد کنید.');
